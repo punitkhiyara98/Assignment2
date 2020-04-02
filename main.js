27b4a6a3f2a03c2b6100f07841db488c1afb567e
Rachel Kim <rkim1238@umd.edu>
2020-04-02 01:47:21 -0400

    js add onclick

--- main.js
+++ main.js
@@ -10,6 +10,9 @@ for (let p = 1; p < 6; p++) {
     const newImage = document.createElement('img');
     newImage.setAttribute('src', "images/pic"+p+".jpg");
     thumbBar.appendChild(newImage);
+    newImage.onClick = function(n) {
+        displayedImage.src = n.target.src;
+    }
 }
 
 
